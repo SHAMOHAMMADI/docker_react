baea21c69cca4a1b8c309a2bd5ecff8bc98ffecd
mustafa <shamohammadi64@gmail.com>
2025-03-08 17:36:01 +0330

    part2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,25 +15,28 @@ function App() {
   }, []);
   return (
     <div>
-      {data.map((result , index)=>(
-        <div key={index} className="[&>*]:text-justify justify-center flex flex-col md:flex-row lg:flex-row items-center border rounded shadow-inner m-4 p-4">
+      {data.map((result, index) => (
+        <div
+          key={index}
+          className="[&>*]:text-justify justify-center flex flex-col md:flex-row lg:flex-row items-center border rounded shadow-inner m-4 p-4"
+        >
           <div className="w-4/12">
-          <img src={result.image} className="w-full " alt="" />
-
+            <img src={result.image} className="w-full " alt="" />
           </div>
           <div className=" [&>*]:text-center w-full">
-          <h1>{result.name}</h1>
-          <p>{result.price}</p>
-          <p>{result.des}</p> 
-          <p>{result.category}</p>
-          <p>{result.rating}</p>
+            <h1>{result.name}</h1>
+            <p>{result.price}</p>
+            <p>{result.des}</p>
+            <p>{result.category}</p>
+            <p>{result.rating}</p>
           </div>
-          <button className="bg-blue-500 w-fit p-2 hover:shadow-inner duration-700 hover:scale-110 text-white shadow-md px-4 rounded-lg">Add to Cart</button>
-          </div>
+          <button className="bg-blue-500 w-fit p-2 hover:shadow-inner duration-700 hover:scale-110 text-white shadow-md px-4 rounded-lg">
+            Add to Cart
+          </button>
+        </div>
       ))}
     </div>
-  
-  )
+  );
 }
 
 export default App;
